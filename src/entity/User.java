8ab5f884097c901516a075700281d3fb86b991e8
Richard Guo <rguo890@gmail.com>
2023-10-19 19:02:35 -0400

    user class beginnings

---
+++ src/entity/User.java
@@ -0,0 +1,40 @@
+package entity;
+import java.util.*;
+public class User {
+    /* User Class, each User is instantiated using their Google credentials*/
+    private final String username;
+    private final String password;
+    private List<User> friends;
+    private int points;
+
+    public User(String uname, String pass, int points){
+        this.username = uname;
+        this.password = pass;
+        this.friends = new ArrayList<>();
+        this.points = points;
+    }
+
+    //Getters
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPassword(){
+        return this.username;
+    }
+    public List<User> getFriends(){
+        return this.friends;
+    }
+    public int getPoints(){
+        return this.points;
+    }
+
+    //Setters
+    public void addFriends(User friend){
+        this.friends.add(friend);
+    }
+    public void addPoints(int points){
+        this.points += points;
+    }
+
+
+}
