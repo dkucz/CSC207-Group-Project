db6b2e659444d3350c2d73e7e69e95876375b3a1
Pranky1247 <113070816+Pranky1247@users.noreply.github.com>
2023-11-24 17:51:20 -0500

    Implemented the CA for button friend.

--- src/Friend/View/Friend_View.java
+++ src/Friend/View/Friend_View.java
@@ -9,11 +9,18 @@ import java.beans.PropertyChangeListener;
 
 public class Friend_View extends JFrame implements PropertyChangeListener {
     private Friend_View_Model Friend_View_Model;
+    private JLayeredPane LP;
     public Friend_View(Friend_View_Model x){
         this.Friend_View_Model = x;
         x.Add_Property_Change_Listener(this);
+        this.setSize(500,1000);
+        this.setLocationRelativeTo(null);
+        I_LP();
     }
-
+    private void I_LP(){
+        this.LP = new JLayeredPane();
+        this.LP.setBounds(0,0,500,1000);
+    }
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
 
--- src/Friend/dataAccess/Friend_Data_Access_Object.java
+++ src/Friend/dataAccess/Friend_Data_Access_Object.java
@@ -1,6 +1,10 @@
 package Friend.dataAccess;
 
 public class Friend_Data_Access_Object implements Friend_Data_Access_Interface{
+    private String IP;
+    public Friend_Data_Access_Object(String IP){
+        this.IP = IP;
+    }
     @Override
     public boolean Exist_By_Name(String Gmail) {
         return false;
